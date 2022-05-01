4f314fa93642486be24add40045abb91a318b827
Josh Bertrand <josh.bertrand@gmail.com>
2022-05-01 14:57:34 -0400

    Moony wolf

--- public/script.js
+++ public/script.js
@@ -1,9 +1,1 @@
-window.onload = function() {
-  const message = document.getElementById("message")
-  document.getElementById("button").addEventListener("click", () => {
-    console.log("hello", message)
-    fetch("/.netlify/functions/hello-world?name=" + "Netlify")
-      .then((data) => data.json())
-      .then(({ msg }) => console.log(msg) || (message.innerHTML = msg))
-  })
-}
+window.onload = function() { }
